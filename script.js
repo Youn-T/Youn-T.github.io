c1e32ff04187bce003016efa469b6d9918b01a13
Youn <yountanguy29@gmail.com>
2024-10-17 04:47:46 +0000

    test

--- script.js
+++
@@ -1,6 +0,0 @@
-function bouton() {
-  oups = document.createElement("p")
-  oups.innerHTML = "Oups !"
-  document.getElementById("logo").parentNode.insertBefore(oups, document.getElementById("logo").nextSibling)
-  document.getElementById("logo").remove()
-}
