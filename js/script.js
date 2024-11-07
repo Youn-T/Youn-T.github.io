466ca2f1655802d7e20caecd0a2027fdcf7a4f91
Youn-T <yountanguy29@gmail.com>
2024-11-07 18:09:01 +0100

    improves carnet section

---
+++ js/script.js
@@ -0,0 +1,12 @@
+import * as data from "../json/carnet.json" assert { type: 'json' };
+
+function LoadCarnetTiles() {
+    // let tilesNumber = 5;
+    // let jsonObjct;
+    console.log(data);
+    // fetch('../json/carnet.json').then((response) => jsonObjct = response.json())
+
+    // // for (let i = 0; i < tilesNumber; i++) {
+
+    // // }
+}
